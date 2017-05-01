0aa156ca47294586d3aa21ea15dc14ebc1df3f76
Anders W. Olsen <anders@wiberg.tech>
2017-05-02 01:17:29 +0200

    one step longer

--- main.cpp
+++ main.cpp
@@ -10,16 +10,46 @@ const char SERIAL_PORT[] = "/dev/ttyPS0";
 
 int openPort(void) {
     int fd = open("/dev/ttyPS0", O_RDWR | O_NOCTTY | O_NDELAY);
+
     if (fd == -1)
         perror("openPort: Unable to serial port.");
     else {
         fcntl(fd, F_SETFL, 0);
+
+
         std::cout << "Success connecting to " << "serial port." << std::endl;
     }
     return(fd);
 }
 
 int main() {
-    openPort();
-    return 0;
+    int             fd = 0;     // File descriptor
+    struct termios  options;    // Terminal options
+    int             rc;         // Return value
+
+    fd = openPort();
+
+    if ((rc = tcgetattr(fd, &options)) < 0) {
+        perror("failed to get attr.");
+        exit(EXIT_FAILURE);
+    }
+
+    cfsetispeed(&options, B115200);
+    cfsetospeed(&options, B115200);
+
+    cfmakeraw(&options);
+    options.c_cflag     |= (CLOCAL | CREAD);   // Enable the receiver and set local mode
+    options.c_cflag     &= ~CSTOPB;            // 1 stop bit
+    options.c_cflag     &= ~CRTSCTS;           // Disable hardware flow control
+    options.c_cc[VMIN]   = 1;
+    options.c_cc[VTIME]  = 2;
+
+    // Set new attributes
+    if ((rc = tcsetattr(fd, TCSANOW, &options)) < 0) {
+        perror("faiuled to set attr.");
+        exit(EXIT_FAILURE);
+    }
+
+    close(fd);
+    return EXIT_SUCCESS;
 }
