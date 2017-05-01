a9b5bebe4f9f4f0f3b57ceb2d164b72271acbfc1
Anders W. Olsen <anders@wiberg.tech>
2017-05-02 01:06:00 +0200

    Removing strings

--- main.cpp
+++ main.cpp
@@ -6,15 +6,15 @@
 #include <errno.h>
 #include <termios.h>
 
-const std::string SERIAL_PORT = "/dev/ttyPS0";
+const char SERIAL_PORT[] = "/dev/ttyPS0";
 
 int openPort(void) {
     int fd = open("/dev/ttyPS0", O_RDWR | O_NOCTTY | O_NDELAY);
     if (fd == -1)
-        perror("openPort: Unable to open " + SERIAL_PORT);
+        perror("openPort: Unable to serial port.");
     else {
         fcntl(fd, F_SETFL, 0);
-        std::cout << "Success connecting to " << SERIAL_PORT << std::endl;
+        std::cout << "Success connecting to " << "serial port." << std::endl;
     }
     return(fd);
 }
